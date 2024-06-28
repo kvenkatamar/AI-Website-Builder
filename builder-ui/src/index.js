0a91bb9db6794b0770e80a21e633141c32b57998
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2024-06-28 19:46:00 +0530

    updated project

--- builder-ui/src/index.js
+++ builder-ui/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import "./index.css";
+import Router from "./routes/routes";
+import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={Router}></RouterProvider>);
